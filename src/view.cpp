4824998b291a86735d07aa3c873b3daa28fab12e
Tony <145881428+tonystdeng@users.noreply.github.com>
2025-01-04 05:58:13 -0500

    Update view.cpp

--- src/view.cpp
+++ src/view.cpp
@@ -9,35 +9,22 @@ Game::Game(char * path)
     //sl::printFile(filetext); // testing
     fileinfo=sl::file2vector(filetext);
     sl::printInfo(fileinfo); // testing
-    double largest=0;
-    double xmax=0,xmin=0,ymax=0,ymin=0;
-    std::vector<sl::pointgraph> parts;
-    sl::point *travelPtr=nullptr;
-    for (int i=0;i<fileinfo.size();i++){
-        std::vector<sl::pointgraph> newParts=sl::getPointGraph(fileinfo[i],travelPtr);
-        parts.insert(parts.end(),newParts.begin(),newParts.end());
-    }
-    for (sl::pointgraph points:parts){
-        for (sl::point i:points){
-            if (i[0]>xmax){xmax=i[0];} else if (i[0]<xmin){xmin=i[0];}
-            if (i[1]>ymax){ymax=i[1];} else if (i[1]<ymin){ymin=i[1];}
-        }
-    }
-    largest=std::max({std::abs(xmax)-std::abs(xmin),std::abs(ymax)-std::abs(ymin)});
-    double resoRate=std::min({width,height})/largest*0.8;
+    std::vector<sl::pointgraph> parts=sl::svg2points(fileinfo);
+
+    std::array<double, 6> repoinfo=sl::getrepoinfo(parts,std::min({width,height}));
+    double xmax=repoinfo[0],xmin=repoinfo[1],ymax=repoinfo[2],ymin=repoinfo[3],resRate=repoinfo[5];
     double reposition=std::min({width,height})*0.1;
+
+    parts=sl::resizeRepo(parts,xmin-reposition,ymin-reposition,resRate);
 
     sf::VertexArray svgshape{sf::LineStrip},simplified{sf::Points};
     for (sl::pointgraph points:parts){
-        for (std::array<double,2>i:points){
-            svgshape.append(sf::Vector2f{(float)((i[0]-xmin)*resoRate+reposition),(float)((i[1]-ymin)*resoRate+reposition)});
-        }svgshapes.push_back(svgshape);
-        svgshape.clear();
+        for (std::array<double,2>i:points){svgshape.append(sf::Vector2f{(float)i[0],(float)i[1]});}
+        svgshapes.push_back(svgshape);  svgshape.clear();
+
         points=sl::simplify(points);
-        for (std::array<double,2>i:points){
-            simplified.append(sf::Vector2f{(float)((i[0]-xmin)*resoRate+reposition)+hw,(float)((i[1]-ymin)*resoRate+reposition)});
-        }simplifieds.push_back(simplified);
-        simplified.clear();
+        for (std::array<double,2>i:points){simplified.append(sf::Vector2f{(float)i[0]+hw,(float)i[1]});}
+        simplifieds.push_back(simplified);  simplified.clear();
     }
     dividLine.setSize(sf::Vector2f(width/256,height));
     dividLine.setPosition({hw-dividLine.getSize().x,0});
